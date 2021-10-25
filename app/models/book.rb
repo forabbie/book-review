1315cf4901576b02ba1e5e32713649048be3b8ee
abbie <abbie@philippinedev.com>
2021-10-25 18:18:55 +0800

    models validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,9 @@ class Book < ApplicationRecord
   belongs_to :category
   has_many :reviews
 
+  validates :title, presence: true
+  validates :author, presence: true
+
   has_attached_file :book_img, styles: { book_index: "300x300>", book_show: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
+
+  validates :rating, presence: true
+  validates :comment, presence: true
 end
